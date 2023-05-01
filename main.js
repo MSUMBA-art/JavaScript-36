ec4c17ca0e95b998d082526a731f6d465415ef95
ALEXANDER <msumba83@gmail.com>
2023-05-01 08:26:38 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+var red = true;
+var blue = false;
+var myNum = 5;
+var myOtherNum = 8;
+var green; // false, null, 0, & undefined returns false
+
+if (myNum == myOtherNum) {
+  console.log('This statement is true');
+} else {
+  console.log('the statement is false');
+}
