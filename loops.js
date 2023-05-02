f7ff0df27de264df0fe67844a0d811d6bc792b0e
ALEXANDER <msumba83@gmail.com>
2023-05-02 06:45:08 +0200

    learn working with strings and arrays in for loop

---
+++ loops.js
@@ -0,0 +1,24 @@
+// FOR LOOP
+
+/* for (var i = 0; i < colors.length; i++) {
+    console.log(colors[i]);
+}*/
+
+//  working with arrays in for loop
+
+/* var colors = ["red", "blue", "yellow", "green", "orange", "purple"]
+
+for (var eachColor of colors) {
+    console.log(eachColor);
+}*/
+
+// working with strings in for loop
+var myString = 'Here I  am testing a string';
+
+for (var i = 0; i < myString.length; i++) {
+    console.log(myString);
+}
+
+for (var eachLetter of myString) {
+    console.log(eachLetter);
+}
